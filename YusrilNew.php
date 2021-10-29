34ec46567ac0f203baaa79b768d78cd6969d47b6
Yusril Ihza Mahendra <93362087+Yusrilihzamahendra@users.noreply.github.com>
2021-10-29 10:38:51 +0700

    PHP1

---
+++ YusrilNew.php
@@ -0,0 +1,15 @@
+<?php
+$a = 50;
+$b = 7;
+$c = 10;
+$d = 17;
+echo "<br/> Identitas Mahasiswa <br/>";
+echo "<br/> Yusril Ihza Mahendra <br/>";
+echo "C1983207019";
+echo "<br/> PTI Semester 5 <br/>";
+echo "<br/> PENJUMLAHAN <br/>";
+    echo $a+$b; //tercetak 57
+    echo "<br/>";
+    $f = $c+$d;
+    echo $f; //tercetak 27
+    echo "<br/>";
