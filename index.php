bb18595cc036c9350527948a30f1bf3566a8f11c
Yusrilihzamahendra <yusriltop6@gmail.com>
2021-10-29 19:33:03 +0700

    Perbaikan koding dan sedikit penambahan informasi

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$a = 50;
+$b = 7;
+$c = 10;
+$d = 17;
+echo "<br/> Identitas Mahasiswa <br/>";
+echo "<br/>Nama : Yusril Ihza Mahendra <br/>";
+echo "NIM : C1983207019";
+echo "Prodi : Pendidikan Teknologi Informasi";
+echo "Semester : 5";
+echo "<br/>Hobi: Main game, Konten kreator yotube, Nonton Anime, Ngoding ringan";
+echo "<br/>Motivasi diri : Jangan malas";
+echo "<br/>TTL : Ciamis, 09, 12, 2000<br/>";
+echo "<br/> PENJUMLAHAN SEDERHANA <br/>";
+	echo "<br/>1. 50 + 7 = ";
+    echo $a+$b; //tercetak 57
+    echo "<br/>";
+	echo "2. 10 + 17 = ";
+    $f = $c+$d;
+    echo $f; //tercetak 27
+    echo "<br/>";
